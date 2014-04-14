f5fbf128189ca0ef40c1191bf4bf3349f78bff48
angelafolz <angelafolz@gmail.com>
2014-04-14 17:58:46 -0500

    jQuery CSS add minor functionality, clean up
    remove outlines on focused elements
    limit number of characters input
    add item with enter key
    disallow empty item, shake on empty input
    (questions in comments after some lines of code)

--- js/app.js
+++ js/app.js
@@ -1,28 +1,43 @@
 $(document).ready(function(){
 
+	var input = $("input[type='text']");
+	var submit = $("input[type='submit']");
+	var list = $(".list");
+
 	// add item to list
-	$("input[type='submit']").click(function(){
-		$(".list").prepend($('<li>' + $('#additem').val() + '<div class="actions"><div class="check"></div><div class="delete"></div><div class="star"></div></div></li>'));
-		$("#additem").val("");
-	})
-	.one("click", function(){
-		$("#additem").removeAttr("placeholder");
+	submit.click(function(){
+		var new_item = input.val();		// this variable is only used in this part of the code, but would it still be better to define it globally so it isn't recreated every time an item is added to the list?
+		if(new_item) {
+			list.prepend($('<li>' + new_item + '<div class="actions"><div class="check"></div><div class="delete"></div><div class="star"></div></div></li>'));		// is it better to stay consistent and use "<div class=\"actions\"> ... " ?
+			input.val("").removeAttr("placeholder");		// I couldn't figure out a way to only run .removeAttr("placeholder") the first time an item is added. Thoughts?
+		}
+		else {
+			input.addClass("shake").focus();		// I couldn't get this to work as a jQuery animation instead of css animation
+			setTimeout(function(){
+				input.removeClass("shake");
+			}, 500);
+		}
+	});
+
+	input.keypress(function(event){
+		if (event.which == 13){
+			submit.click();
+		}
 	});
 
 	// check and uncheck item
-	$(".list").on("click", ".check", function(){
+	list.on("click", ".check", function(){
 		$(this).closest("li").toggleClass("checked");
 	});
 
 	// star and unstar item
-	$(".list").on("click", ".star", function(){
+	list.on("click", ".star", function(){
 		$(this).closest("li").toggleClass("starred");
 	});
 
 	// remove item from list
-	$(".list").on("click", ".delete", function(){
+	list.on("click", ".delete", function(){
 		$(this).closest("li").remove();
 	});
 
 });
-
